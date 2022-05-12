ba33950a69f53da20489756011ea76c3c8bb5290
Cristian <cristianbuiles91@gmail.com>
2022-05-11 23:02:38 -0500

    php.2

---
+++ insertar.php
@@ -0,0 +1,16 @@
+<?php
+$conexion = mysqli_connect("localhost","root","","bd_hotel sweet dreams");
+
+$nombre = $_POST["nombre"];
+$correo = $_POST["correo"];
+$telefono = $_POST["telefono"];
+$destino = $_POST["destino"];
+$mensaje = $_POST["mensaje"];
+
+    $insertarSQL = "INSERT INTO reservas(nombre, correo, telefono, destino, mensaje) VALUES ('$nombre','$correo','$telefono','$destino','$mensaje')";
+    $resultado = mysqli_query($conexion,$insertarSQL);
+    if($resultado){
+        echo "<script>alert('se ha enviado su reserva'); window.location='index.html'</script>";
+    }else{
+        printf("Errormessage: %s\n",mysqli_error($conexion));
+    }
