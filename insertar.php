45bea92edae4d61761268424bd2d9f0b9bbd22a8
Cristian <cristianbuiles91@gmail.com>
2022-05-13 20:43:11 -0500

    Final

--- insertar.php
+++ insertar.php
@@ -5,9 +5,11 @@ $nombre = $_POST["nombre"];
 $correo = $_POST["correo"];
 $telefono = $_POST["telefono"];
 $destino = $_POST["destino"];
-$mensaje = $_POST["mensaje"];
+$dias = $_POST["dias"];
+$valor = $_POST["valor"];
+$descuento = $_POST["descuento"];
 
-    $insertarSQL = "INSERT INTO reservas(nombre, correo, telefono, destino, mensaje) VALUES ('$nombre','$correo','$telefono','$destino','$mensaje')";
+    $insertarSQL = "INSERT INTO reservas(nombre, correo, telefono, destino, dias, valor, descuento) VALUES ('$nombre','$correo','$telefono','$destino','$dias','$valor', '$descuento')";
     $resultado = mysqli_query($conexion,$insertarSQL);
     if($resultado){
         echo "<script>alert('se ha enviado su reserva'); window.location='index.html'</script>";
